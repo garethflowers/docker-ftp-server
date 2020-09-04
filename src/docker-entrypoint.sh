5aa66a69265745f5372eb672f0319d01c01c9261
Gareth Flowers <garethflowers@users.noreply.github.com>
2020-09-04 22:50:49 +0100

    fix(): generate same user and group

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+addgroup \
+	-S \
+	$FTP_USER
 
 adduser \
 	-D \
-	-G ftp \
+	-G $FTP_USER \
 	-h /home/$FTP_USER \
 	-s /bin/false \
 	$FTP_USER
 
 mkdir -p /home/$FTP_USER
-chown -R $FTP_USER:ftp /home/$FTP_USER
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
 
 exec "$@"
