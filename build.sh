ae2b667403e450c74b6d6e6f1aee610bffc7ef95
Gareth Flowers <garethflowers@users.noreply.github.com>
2021-04-12 22:55:09 +0100

    refact(): removes temp build script

--- build.sh
+++
@@ -1,3 +0,0 @@
-docker build --no-cache --tag ftp .
-docker rm -fv ftp
-# docker run -d -p 20-21:20-21/tcp -p 40000-40009:40000-40009/tcp --env FTP_USER=ftp-user --env FTP_PASS=ftp-pass --name ftp --restart always docker.io/garethflowers/ftp-server:0.1.0
