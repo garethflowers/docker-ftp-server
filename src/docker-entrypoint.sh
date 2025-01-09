cf944a26295ebc35b2087d4bb0e663030267c315
Gareth Flowers <garethflowers@users.noreply.github.com>
2025-01-09 22:15:36 +0000

    feat(): use existing user group when possible (#102)

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
+FTP_DATA=/home/$FTP_USER
+FTP_GROUP=$( getent group "$GID" | awk -F ':' '{print $1}' )
 
-addgroup \
-	-g $GID \
-	-S \
-	$FTP_USER
+if test -z "$FTP_GROUP"; then
+	FTP_GROUP=$FTP_USER
+	addgroup -g "$GID" -S "$FTP_GROUP"
+fi
 
-adduser \
-	-D \
-	-G $FTP_USER \
-	-h /home/$FTP_USER \
-	-s /bin/false \
-	-u $UID \
-	$FTP_USER
+adduser -D -G "$FTP_GROUP" -h "$FTP_DATA" -s "/bin/false" -u "$UID" "$FTP_USER"
 
-mkdir -p /home/$FTP_USER
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+mkdir -p $FTP_DATA
+chown -R "$FTP_USER:$FTP_GROUP" "$FTP_DATA"
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
 
 sed -i -r "s/0.0.0.0/$PUBLIC_IP/g" /etc/vsftpd.conf
